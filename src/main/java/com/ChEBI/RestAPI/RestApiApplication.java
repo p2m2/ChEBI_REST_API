206b94b69eaa2203bfbc2f4fe5cd27c3643a2cb9
M.Palerme <marcellino.palerme@inrae.fr>
2024-10-21 15:33:06 +0200

    Structure generated by java spring

---
+++ src/main/java/com/ChEBI/RestAPI/RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ChEBI.RestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+}
